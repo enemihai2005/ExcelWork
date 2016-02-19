03984db726a2824b2d0a42d9b900289394328501
Ene Mihai <enemihai2005@yahoo.com>
2016-02-19 08:52:19 -0500

    upload.php modified

--- view/upload.php
+++ view/upload.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: MIKE
- * Date: 2/16/16
- * Time: 9:05 AM
- * To change this template use File | Settings | File Templates.
- */
 
 
 $target_dir = "uploads/";
@@ -14,5 +7,6 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 } else {
+
     echo "Sorry, there was an error uploading your file.";
 }
