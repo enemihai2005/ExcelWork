0c57fb490b28649b497526e1947487481ac6e003
spyre <spirescualex@gmail.com>
2016-02-19 17:14:30 +0200

    MODIFIED SOMETHING

--- view/upload.php
+++ view/upload.php
@@ -10,6 +10,6 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 } else {
-
+	// added one line
     echo "Sorry, there was an error uploading your file.";
 }
